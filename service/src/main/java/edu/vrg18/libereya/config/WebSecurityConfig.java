9aa884ed3312dc76f46ccf5952646fa723204033
Valeriy Grigoryev <grig1e@mail.ru>
2019-08-19 19:11:18 +0400

    Мелкие испр.

--- service/src/main/java/edu/vrg18/libereya/config/WebSecurityConfig.java
+++ service/src/main/java/edu/vrg18/libereya/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
@@ -38,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
-    @Autowired
+    @Resource
     private void setUserDetailsService(UserDetailsServiceImpl userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
